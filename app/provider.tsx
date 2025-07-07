ec228c6fabf97bf57191f6f8effd22589a7fd830
italha0 <tq74198@gmail.com>
2025-07-07 10:58:24 +0530

    Update provider.tsx

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+// import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
